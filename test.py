cf4d3a026b08385cd04c1b04729af974ef4da435
maruko0713 <wanzifun@gmail.com>
2017-02-08 13:45:57 +0800

    create application

---
+++ test.py
@@ -0,0 +1,12 @@
+#-*- encoding=UTF-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "hello"
+
+if __name__ ==  "__main__":
+    app.run(debug=True)
